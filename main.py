fae2982b93f82211cce07f921264c034642dd928
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2017-04-17 15:22:50 +0500

    Re-engineered the code to use sqlite as a DB Backend and store values. Used Pony ORM for the work.
    Signed-off-by: Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>

--- main.py
+++ main.py
@@ -3,21 +3,36 @@ This file contains the basic code to download and store all the data for the 5th
 Website. This site achieves that by doing the following:
 
 1. It runs through the entire list of possible roll numbers from '00-000-000' to '99-999-999'.
-2. After every page load, it checks if a result was returned. If it was, it stores that result into a local dict indexed
-by the roll number.
-3. After the run is complete, it takes the dict and then processes it into a dataframe.
+2. After every page load, it checks if a result was returned. If it was, it stores that result into a local sqplite db indexed
+by the roll number, otherwise it commits a NULL to it.
+3. After the run is complete, it takes the db and processes the data.
 """
 
-import os
 from collections import namedtuple
 
-from bs4 import BeautifulSoup
+from pony import orm
 from splinter import Browser
 
 RollNo = namedtuple("RollNo", ['roll_no1', 'roll_no2', 'roll_no3'])
+DBNAME = "data.sqlite"
+
+db = orm.Database()
+db.bind('sqlite', DBNAME, create_db=True)
 
 
-def visit(browser, url, rollno, invalid, outdict, outlist):
+class Record(db.Entity):
+    rollno1 = orm.Required(str)
+    rollno2 = orm.Required(str)
+    rollno3 = orm.Required(str)
+    html = orm.Required(str)
+    error = orm.Required(bool)
+
+
+db.generate_mapping(create_tables=True)
+
+
+@orm.db_session
+def visit(browser, url, rollno, invalid):
     """
     This method visits the given site, fills the form, checks if a valid result is generated, adds the valid result 
     and the valid roll number to the valid dict and valid list respectively.
@@ -26,53 +41,30 @@ def visit(browser, url, rollno, invalid, outdict, outlist):
     :param url: 
     :param rollno: 
     :param invalid: 
-    :param outdict: 
-    :param outlist: 
-    :return: 
     """
     browser.visit(url)
     browser.fill_form(rollno._asdict())
     browser.find_by_name("search").click()
     if browser.is_text_present(invalid):
-        pass
+        Record(rollno1=rollno[0], rollno2=rollno[1], rollno3=rollno[2], html="NULL", error=True)
     else:
-        outlist.push(rollno)
-        outdict['-'.join(rollno)] = browser.html
-        with open(os.path.join("dumpdir", '-'.join(rollno))) as f:
-            f.write(browser.html)
+        Record(rollno1=rollno[0], rollno2=rollno[1], rollno3=rollno[2], html=browser.html, error=False)
 
 
-def process_data(results):
-    """
-    Iterate through the list of results, extract the data and return it in a list of namedtuples.
-    :param results: 
-    :return: list of namedtuples.
-    """
-
-    result_only = {}
-    for key, item in results.items():
-        soup = BeautifulSoup(item, "lxml")
-        soup = soup.find("div", class_="container")
-        result_only[key] = soup
-
-    return result_only
-
-
-def generate_local_dump(validlist, validdict, outfiletemplate):
-    """
-    Dumps the list of valid roll numbers to the given file.
-    
-    :param validlist: 
-    :param validdict: 
-    :param outfiletemplate: 
-    :return: 
-    """
-    with open(outfiletemplate.format("nums"), 'w') as f:
-        f.write('\n'.join(validlist))
-
-
-# with open(outfiletemplate.format("html"), 'w') as f:
-#        f.write('\n'.join(validdict.values))
+# def process_data(results):
+#     """
+#     Iterate through the list of results, extract the data and return it in a list of namedtuples.
+#     :param results:
+#     :return: list of namedtuples.
+#     """
+#
+#     result_only = {}
+#     for key, item in results.items():
+#         soup = BeautifulSoup(item, "lxml")
+#         soup = soup.find("div", class_="container")
+#         result_only[key] = soup
+#
+#     return result_only
 
 def download_data():
     rn1 = [str(x).zfill(2) for x in range(0, 100)]
@@ -81,21 +73,22 @@ def download_data():
 
     rnlist = [RollNo(a, b, c) for a in rn1 for b in rn2 for c in rn3]
 
-    url = "http://pec.edu.pk"
-    driver = "chrome"
-    valid_roll_no = []
+    URL = "http://pec.edu.pk"
+    DRIVER = "chrome"
+    INVALID_RESULT = "No Result found"
+    BROWSER = Browser(DRIVER)
+    with orm.db_session:
+        last_record = orm.max(r.id for r in Record)
+        if last_record:
+            last_roll_num = RollNo(
+                *orm.select((r.rollno1, r.rollno2, r.rollno3) for r in Record if r.id == last_record)[:][0])
+            start = rnlist.index(last_roll_num) + 1
+        else:
+            start = 0
 
-    valid_results = {}
-    invalid_result = "No Result found"
-
-    browser = Browser(driver)
-    for rn in rnlist:
-        visit(browser, url, rn, invalid=invalid_result, outlist=valid_roll_no, outdict=valid_results)
-
-    return (valid_roll_no, valid_results)
+    print("Starting the Brute Force Search from position {}".format(start))
+    for rn in rnlist[start:]:
+        visit(BROWSER, URL, rn, invalid=INVALID_RESULT)
 
 
-valid_roll_no_filename = os.path.join("data", "valids.{}.txt")
-rns, rndata = download_data()
-rnpdata = process_data(rndata)
-generate_local_dump(rns, rnpdata, valid_roll_no_filename)
+download_data()
