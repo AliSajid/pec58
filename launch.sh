99d6f277c37c85c70c1c2347cb3e8d429b637038
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2017-04-17 23:07:53 +0500

    Making sure that the script runs exactly as expected.
    Signed-off-by: Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>

--- launch.sh
+++ launch.sh
@@ -4,5 +4,5 @@ read -a nums <<<$(seq 0 625000 10000000)
 
 for ((i=0; i <= 15; i++))
 do
-    echo "nohup python main.py -s ${nums[i]} -e ${nums[i+1]} && >> data-"${nums[i]}"-"${nums[i+1]}".log"
+    echo "nohup python main.py -s ${nums[i]} -e ${nums[i+1]} >> logs/data-"${nums[i]}"-"${nums[i+1]}".log &"
 done
